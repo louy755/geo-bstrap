4458f28e3e437da9ac1e304265a533342caadc89
louy <lumberjack1695@yahoo.com>
2017-04-30 03:31:39 +0000

    adding user name to the map pointer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
-  marker.lat user.latitude
-  marker.lng user.longitude
+     marker.lat user.latitude
+     marker.lng user.longitude
+     marker.infowindow user.name
     end
   end
 
